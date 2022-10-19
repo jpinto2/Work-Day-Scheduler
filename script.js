d8ea7f44741e69d589b02462d82e9e401f6c95f4
jpinto3 <jpinto3@mail.csuchico.edu>
2022-10-18 19:42:51 -0700

    added comments

--- script.js
+++ script.js
@@ -2,21 +2,30 @@ var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
 $(function ()  {
+    // when save button for a row is clicked
     $(".saveBtn").on("click", function () {
 
+        // get time id of the save buttons parent div
         var time = $(this).parent().attr("id");
+        // get value typed into the description textarea for that save button 
         var text = $(this).siblings(".description").val();
+        // store that text in localstorage for that time id
         localStorage.setItem(time, text);
 
     })
-   
+
+    // function to set past, present, future class to each time block. 
     function currentTime() {
         
+        // current hour in military time
         var time = moment().hour();
+        // inital hour on planner. tried to initially parseInt the id of the timeblock but was getting issues
         var blockTime = 9;
- 
+        
+        // jquery function to iterate over every section with time-block class. initially planned to use for loop but stumbled over this thing in jquery documentation and researched it on google"
         $(".time-block").each(function () {
 
+            // jquery function to remove class. no issue with removing non existent class or adding existing class, so i just called remove on both classes i didn't want from style.css and called add on the one i wanted.
             if (blockTime < time) {
                 $(this).removeClass("present");
                 $(this).removeClass("future");
@@ -33,11 +42,13 @@ $(function ()  {
                 $(this).addClass("future");
 
             }
+            // increment to next time block
             blockTime++;
         })
     }
 
    
+    //gets any locally stored values in descriptions and displays them on web page in textarea
     $("#9 .description").val(localStorage.getItem("9"));
     $("#10 .description").val(localStorage.getItem("10"));
     $("#11 .description").val(localStorage.getItem("11"));
@@ -48,5 +59,6 @@ $(function ()  {
     $("#16 .description").val(localStorage.getItem("16"));
     $("#17 .description").val(localStorage.getItem("17"));
 
+    // applies past, present, future classes to each time block
     currentTime();
 })
