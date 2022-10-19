3e73c2f374e7c46150c6027142484169a0730535
jpinto3 <jpinto3@mail.csuchico.edu>
2022-10-18 19:15:19 -0700

    webpage working and corrected display errors i missed

--- script.js
+++ script.js
@@ -1,2 +1,52 @@
 var today = moment();
-$("#currentDay").text(today.format("MMM Do, YYYY"));
+$("#currentDay").text(today.format("dddd, MMMM Do"));
+
+$(function ()  {
+    $(".saveBtn").on("click", function () {
+
+        var time = $(this).parent().attr("id");
+        var text = $(this).siblings(".description").val();
+        localStorage.setItem(time, text);
+
+    })
+   
+    function currentTime() {
+        
+        var time = moment().hour();
+        var blockTime = 9;
+ 
+        $(".time-block").each(function () {
+
+            if (blockTime < time) {
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+                $(this).addClass("past");
+            }
+            else if (blockTime === time) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+
+            }
+            blockTime++;
+        })
+    }
+
+   
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
+
+    currentTime();
+})
